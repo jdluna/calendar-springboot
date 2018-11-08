cfae803d0c545c2bda471480aac580a8dfb5726e
Karanveer <karan_4496@hotmail.com>
2018-11-08 16:49:22 -0500

    First Commit

---
+++ src/main/java/edu/sollers/javaprog/CalendarProjectApplication.java
@@ -0,0 +1,12 @@
+package edu.sollers.javaprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/sollers/javaprog/CalendarProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.sollers.javaprog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalendarProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
